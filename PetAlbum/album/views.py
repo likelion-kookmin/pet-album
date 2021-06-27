327468842e0c3cc3e294435e2c9bf9f3c98f6d2e
jngsjn <firstarting@gmail.com>
2021-06-27 22:24:28 +0900

    edit urls.py
    슬라이드쇼 사진 연결

--- PetAlbum/album/views.py
+++ PetAlbum/album/views.py
@@ -4,7 +4,7 @@ from .models import Album
 
 def album(request):
   albums = Album.objects.all()
-  return render(request, 'album.html')
+  return render(request, 'album.html', {'albums':albums})
 
 def album_add(request):
   return render(request, 'album_add.html')
