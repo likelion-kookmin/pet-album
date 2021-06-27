d8025ef424ad6e4b9fac060a4d140790edde6a46
jngsjn <firstarting@gmail.com>
2021-06-27 20:37:53 +0900

    add models.py
    모델 초기 세팅

--- PetAlbum/album/models.py
+++ PetAlbum/album/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    user_id = models.ForeignKey
+    pet_id = models.ForeignKey
+    album_image = models.ImageField(upload_to ="image_upload_path", null=True, blank=True)
+    comment = models.TextField(null=True)
+    image_datetime = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=True)
