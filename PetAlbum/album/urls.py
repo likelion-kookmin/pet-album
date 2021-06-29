c424ac7583d2c5b0a72e2e516392f7c2f5a5223a
jngsjn <firstarting@gmail.com>
2021-06-29 17:37:40 +0900

    add album_edit.html
    album_edit.html 분리

--- PetAlbum/album/urls.py
+++ PetAlbum/album/urls.py
@@ -6,5 +6,7 @@ app_name = 'album'
 urlpatterns = [
   path('', views.album, name = "album"),
   path('add/', views.album_add, name = "album_add"),
+  path('edit/<str:id>', views.album_edit, name = 'album_edit'),
+  path('update/<str:id>', views.album_update, name = 'album_update'),
 ]
 
--- PetAlbum/album/views.py
+++ PetAlbum/album/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Album
 # Create your views here.
 
@@ -8,4 +8,16 @@ def album(request):
 
 def album_add(request):
   albums = Album.objects.all()
-  return render(request, 'album_add.html',{'albums':albums})
+  return render(request, 'album_add.html', {'albums':albums})
+
+def album_edit(request, id):
+  albums = Album.objects.all()
+  edit_album = get_object_or_404(Album, pk = id)
+  return render(request, 'album_edit.html', {'albums':albums, 'album':edit_album})
+
+def album_update(request, id):
+  update_album = get_object_or_404(Album, pk = id)
+  update_album.comment = request.POST['comment']
+  update_album.is_public = request.POST['is_public']
+  update_album.save()
+  return redirect('album_add')
