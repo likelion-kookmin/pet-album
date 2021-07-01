fada03bdac64f33e7f96c904fb4b00513936c4c8
ssuppark <suhyeon0107@naver.com>
2021-07-01 19:52:23 +0900

    pet 브랜치  충돌 해결

--- PetAlbum/album/urls.py
+++ PetAlbum/album/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from . import views
+import album.views as views
 
 # app_name = 'album'
 
 urlpatterns = [
-  path('', views.album, name = "album"), # all user albums
   path('<int:pet_id>', views.album_pet, name = "album_pet"), # specific user + specific pet
   path('<int:pet_id>/add/', views.album_add, name = "album_add"),
   path('<int:pet_id>/create/', views.album_create, name='album_create'),
